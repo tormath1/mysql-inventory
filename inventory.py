ac34822b46b9af2176cb1d8d33d115420ac676ba
Mathieu Tortuyaux <mathieu.tortuyaux@gmail.com>
2020-08-27 12:21:35 +0200

    inventory: add comments

--- inventory.py
+++ inventory.py
@@ -5,6 +5,8 @@ import json
 
 import mysql.connector
 
+# MySQL server connection information
+# TODO: could be loaded from a configuration file
 host = os.getenv("ANSIBLE_INVENTORY_MYQSL_HOST", "localhost")
 port = os.getenv("ANSIBLE_INVENTORY_MYSQL_PORT", 3306)
 username = os.getenv("ANSIBLE_INVENTORY_MYSQL_USERNAME", "username")
@@ -22,7 +24,9 @@ class Inventory(object):
         database,
         ssl_skip_verify,
     ):
+        # define base inventory
         self.inventory = {"group":{}, "_meta": {}}
+        # disable or not SSL CA verification
         ssl_verify = not ssl_skip_verify
         try:
             self.conn = mysql.connector.connect(
@@ -43,13 +47,22 @@ class Inventory(object):
         query = "SELECT * FROM server_physique;"
         with self.conn.cursor(dictionary=True) as cur:
             cur.execute(query)
+            # fetchall will return a dictionary,
+            # each column is a key of the dictionary
             res = cur.fetchall()
+        # prepare the default group
         self.inventory["group"]["hosts"] = []
         self.inventory["_meta"]["hostvars"] = {}
         for server in res:
+            # we save and remove the `hostname` from the
+            # json. It's removed to avoid having `hostname`
+            # in the hostvars
             hostname = server.pop("hostname")
             self.inventory["group"]["hosts"].append(hostname)
+            # we add the server information to hostvars
             self.inventory["_meta"]["hostvars"][hostname] = server
+            # custom override to set the `ansible_host` to the baseuri in order
+            # to connect
             self.inventory["_meta"]["hostvars"][hostname]["ansible_host"] = \
                 server.get("baseuri")
         return json.dumps(self.inventory, indent=2)
@@ -66,4 +79,5 @@ if __name__ == "__main__":
         )
         print(i.generate())
     except:
+        # TODO: handle the error
         pass
